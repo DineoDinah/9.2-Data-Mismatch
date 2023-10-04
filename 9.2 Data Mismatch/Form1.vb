0dd281b3768e898638d2203879e23ab3fca018b5
Lenovo-User <Lenovo-User@DESKTOP-RNPFAS5>
2023-10-04 18:21:14 +0200

    Add project files.

---
+++ 9.2 Data Mismatch/Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+	Private Sub BtnShow_Click(sender As Object, e As EventArgs) Handles BtnShow.Click
+		Dim text1 As String
+		Dim Text2 As Integer
+		Dim text3 As String
+
+		text1 = "Visual"
+		Text2 = 22
+		text3 = text1 & Text2
+		MsgBox(text3)
+	End Sub
+End Class
